32b730c96588f79a471df3cc8bb9ed8323ec9b91
Muslim Beibytuly <muslimbeibytuly@gmail.com>
2018-01-30 11:02:18 +0600

    snake mvp

--- Snake/Source.cpp
+++ Snake/Source.cpp
@@ -19,20 +19,20 @@ struct Food
 	Point body;
 	char sign = '#';
 };
-void draw(Point & p, char sign) 
+void draw(Point & p, char sign)
 {
 	COORD position = { p.x, p.y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
 	cout << sign;
 }
-void drawEverything(Snake & snake, Food & food) 
+void drawEverything(Snake & snake, Food & food)
 {
 	for (int i = 0; i < snake.length; ++i) {
 		draw(snake.body[i], snake.sign);
 	}
 	draw(food.body, food.sign);
 }
-void generation(Snake & snake, Food & food) 
+void generation(Snake & snake, Food & food)
 {
 	snake.body = new Point[20];
 	snake.body[0].x = 3;
@@ -41,7 +41,7 @@ void generation(Snake & snake, Food & food)
 	food.body.x = 5;
 	food.body.y = 5;
 }
-void snakeMove(Snake & snake, int dx, int dy) 
+void snakeMove(Snake & snake, int dx, int dy)
 {
 	//остальные подтягиваются
 	for (int i = snake.length; i > 0; --i) {
@@ -52,13 +52,38 @@ void snakeMove(Snake & snake, int dx, int dy)
 	snake.body[0].x += dx;
 	snake.body[0].y += dy;
 }
-
+bool checkIfSnakeEatsFood(Snake & snake, Food & food)
+{
+	if (snake.body[0].x == food.body.x && snake.body[0].y == food.body.y)
+	{
+		return true;
+	}
+	else { return false; }
+}
+void snakeEat(Snake & snake, Food & food) {
+	snake.body[snake.length].x = food.body.x;
+	snake.body[snake.length].y = food.body.y;
+	++snake.length;
+}
+void generateFood(Snake & snake, Food & food)
+{
+	food.body.x = rand() % 20 + 5;
+	food.body.y = rand() % 20 + 5;
+	for (int i = 0; i < snake.length; ++i)
+	{
+		if (food.body.x == snake.body[i].x && food.body.y == snake.body[i].y) 
+		{
+			generateFood(snake, food);
+		}
+	}
+}
 void game()
 {
 	Snake snake;
 	Food food;
 	generation(snake, food);
-	while (true) {
+	while (true)
+	{
 		drawEverything(snake, food);
 		char action = _getch();
 		switch (action)
@@ -79,6 +104,11 @@ void game()
 			return;
 		default:
 			break;
+		}
+		if (checkIfSnakeEatsFood(snake, food))
+		{
+			snakeEat(snake, food);
+			generateFood(snake, food);
 		}
 		system("cls");
 	}
